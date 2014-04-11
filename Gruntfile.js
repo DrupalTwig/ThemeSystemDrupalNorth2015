ba7b0d3f88f327fdd23fcf3aa2c513b8c6f9c890
Joel Pittet <joel@pittet.ca>
2014-04-11 18:41:05 -0400

    styles and webfont

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,9 +47,9 @@ module.exports = function(grunt) {
 					// 'css/theme/simple.css': 'css/theme/source/simple.scss',
 					// 'css/theme/sky.css': 'css/theme/source/sky.scss',
 					// 'css/theme/moon.css': 'css/theme/source/moon.scss',
-					'css/theme/solarized.css': 'css/theme/source/solarized.scss',
-					'css/theme/theme-system.css': 'css/theme/source/theme-system.scss',
+					// 'css/theme/solarized.css': 'css/theme/source/solarized.scss',
 					// 'css/theme/blood.css': 'css/theme/source/blood.scss'
+					'css/theme/theme-system.css': 'css/theme/source/theme-system.scss'
 				}
 			}
 		},
